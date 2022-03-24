c5ffb38f05f8f00ac47c1770b754975c4723d78a
yang-wenxi <wd.yang@live.com>
2022-03-24 22:07:01 +0800

    emm

--- Source/SuperSampler.h
+++ Source/SuperSampler.h
@@ -11,4 +11,6 @@
 #pragma once
 
 #include <JuceHeader.h>
-juce::String fuckYouGitHub{ "You are just full of shit" };
+juce::String fuckYouGitHub { "You are just full of shit" };
+
+juce::String motherFucker {"Yeah motherfucker github screw you"};
